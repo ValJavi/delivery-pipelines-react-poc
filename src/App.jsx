5c29fdb65a6520c6a38fefce63825b32487b8c1e
Javier Valdez <javier.valdez@telus.com>
2026-09-15 10:21:51 -0300

    fix: use greeting variable instead of leaving it unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
-  const unusedGreeting = "Hola";
+  const greeting = "Hola";
 
   return (
     <main>
       <h1>Delivery Pipelines POC</h1>
+      <p>{greeting}, TD Bootcamp!</p>
     </main>
   )
 }
