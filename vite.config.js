12a1ffef7a3c180c41852f5980ae6a03bb53df70
Javier Valdez <javier.valdez@telus.com>
2026-09-15 11:17:24 -0300

    fix: remove invalid build entry override

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'index-production.html',
-    },
-  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
