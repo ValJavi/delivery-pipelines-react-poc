fc016b570fcd2b3461c90784c0bb5c6bccce05f8
Javier Valdez <javier.valdez@telus.com>
2026-09-15 11:12:51 -0300

    fix: point build entry to nonexistent HTML file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index-production.html',
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
